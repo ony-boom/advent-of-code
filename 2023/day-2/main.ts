757f5f364c74023e360a827075d68778a0f52b52
Ony <orakotonirina@bocasay.com>
2023-12-05 17:03:19 +0300

    finish 2023 day 2

--- 2023/day-2/main.ts
+++ 2023/day-2/main.ts
@@ -1,8 +1,3 @@
-/**
- * Determine which games would have been possible if the bag had been loaded with only 12 red cubes, 13 green cubes, and 14
- * blue cubes. What is the sum of the IDs of those games?
- */
-
 import { getInputLines } from "../../shared/utils.ts";
 
 const lines = await getInputLines(2023, 2);
