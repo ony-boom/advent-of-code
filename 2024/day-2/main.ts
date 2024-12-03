f373a2069d297ccd0baa8a197f0180a0762960c0
Ony <onyrakoto27@gmail.com>
2024-12-03 17:15:02 +0300

    d2

--- 2024/day-2/main.ts
+++ 2024/day-2/main.ts
@@ -2,40 +2,41 @@ import { getInputLines } from "@utils";
 
 const lines = await getInputLines(2024, 2);
 
-function checkSafety(line: number[]) {
-    if (lines.length < 2) {
-        return true;
+const isListSafe = (line: number[]) => {
+  const checkDirection = (ascending: boolean) =>
+    line.every((val, idx) =>
+      idx === line.length - 1 ||
+      (ascending ? line[idx + 1] - val : val - line[idx + 1]) >= 1 &&
+        (ascending ? line[idx + 1] - val : val - line[idx + 1]) <= 3
+    );
+
+  return checkDirection(true) || checkDirection(false);
+};
+
+const checkSafety = (line: number[]) => {
+  if (isListSafe(line)) {
+    return true;
+  }
+
+  for (let i = 0; i < line.length; i++) {
+    const newLine = [...line.slice(0, i), ...line.slice(i + 1)];
+    if (isListSafe(newLine)) {
+      return true;
     }
+  }
 
-    let increasing = true;
-    let decreasing = true;
+  return false;
+};
 
-    for (let i = 0; i < line.length - 1; i++) {
-        if (line[i] >= line[i + 1]) {
-            increasing = false;
-        }
-        if (line[i] <= line[i + 1]) {
-            decreasing = false;
-        }
+export const getSafeReports = () => {
+  let safeReportCount = 0;
 
-        const diff = Math.abs(line[i] - line[i + 1]);
-
-        if (!increasing && !decreasing || diff > 3) {
-            return false;
-        }
-    }
-
-    return increasing || decreasing;
-}
-
-let safeReportCount = 0;
-
-for (const line of lines) {
+  for (const line of lines) {
     const report = line.split(" ").map(Number);
     const isSafe = checkSafety(report);
     if (isSafe) {
-        safeReportCount++;
+      safeReportCount++;
     }
-}
-
-console.log(safeReportCount);
+  }
+  return safeReportCount;
+};
